8df71a41cbf2e4afe20d7be53a36e9c9bf270c75
Thiago Daisuke Ozawa <yorualptraum@gmail.com>
2023-12-10 22:16:38 -0300

    added initial cube
    added initial cube and the starting design, still a wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>Yoru Alptraum</title>
+
+<script>
+    import Cube from './cube.svelte';
+    import './style.css';
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    window.onresize = () => {
+        width = window.outerWidth;
+        height = window.outerHeight;
+    };
+</script>
+
+<div class="hero">
+    <div class="info">
+        <h1>Yoru Alptraum</h1>
+        <h2>Software Developer w{width}h{height}</h2>
+    </div>
+    <div id="anim">
+        <Cube w={width*0.4} h={height*0.4} color="#6900FD"/>
+    </div>
+</div>
