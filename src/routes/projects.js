1b3545d0faf67de44132b484183259b05efda3ca
Thiago Daisuke Ozawa <yorualptraum@gmail.com>
2024-07-21 11:53:38 -0300

    Update projects.js
    Updated text about no splash for opera gx

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,12 +1,12 @@
 // template for new projects
 
-    // {
-    //     "link": "",
-    //     "img": "",
-    //     "title": "",
-    //     "inner": "",
-    //     "techs": [""]
-    // },
+// {
+//     "link": "",
+//     "img": "",
+//     "title": "",
+//     "inner": "",
+//     "techs": [""]
+// },
 
 export const projectList = [
     {
@@ -14,27 +14,27 @@ export const projectList = [
         "img": "./capy_notes.png",
         "title": "Capybara Notes",
         "inner": "Capybara Notes is a application that helps fill  MDS (Minimum Data Set) templates. <br /><br /> I created this app to assist with work in Service Desk because I felt that I needed a better way to fill work notes than just editing a text template. <br /><br/> After completing the app, some of my colleagues also showed interest and started using it for work. This provided me with valuable feedback and insight into how others perceived the app I envisioned.",
-        "techs": ["Python","Tkinter","py2exe"]
+        "techs": ["Python", "Tkinter", "py2exe"]
     },
     {
         "link": "https://github.com/YoruAlptraum/no-splash-opera-gx",
         "img": "./no_splash.png",
         "title": "No splash for Opera GX",
-        "inner": "This script intercepts Opera GX shortcuts and executes a script that deletes the splash screen exe from latest release folder before launching the app to completely prevent the browser splash screen animation. <br/><br/> A very small and simple project but it is a project that has helped me learn a lot about what scripting applications can really do.",
-        "techs": ["VBScript","batch","regex"]
+        "inner": "This script intercepts Opera GX taskbar and Start Menu shortcuts on Windows and executes a script that deletes the splash screen exe from release folder before launching the app, completely preventing Opera GX splash screen animation. <br/><br/> A very small and simple project but it's a project that has helped me learn a lot about what scripting applications can really do.",
+        "techs": ["VBScript", "batch", "Windows"]
     },
     {
         "link": "https://github.com/YoruAlptraum/web-scraper-tests",
         "img": "./web_scraping.png",
         "title": "Web Scraping",
         "inner": "Extracting, cleaning and exporting website data to Excel for easy data visualization and utilization.<br /><br /> Using beautiful soup it's easy to retrieve data from a website but it takes some time to check for inconsistencies on the site structure and clean up the data. <br /><br /> By extracting data from the site I was also able to automate the download of all site images to a local folder.",
-        "techs": ["Python","Beautiful Soup","Pandas","Google Colab"]
+        "techs": ["Python", "Beautiful Soup", "Pandas", "Google Colab"]
     },
     {
         "link": "https://github.com/YoruAlptraum/pit-senai-v2",
         "img": "./pit.png",
         "title": "PIT SENAI",
         "inner": "The PIT is the final project for the IT course in SENAI. <br /><br /> For my final project, I made a simple stock management app that allows for the registration of new items or clients as well as different levels of user access to manage the app.",
-        "techs": ["C#","Windows Forms","MySQL"]
+        "techs": ["C#", "Windows Forms", "MySQL"]
     }
 ]
